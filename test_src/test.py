d82174b0264f2825f706c8a754bc4601896769f3
sr.morteza <sr.morteza@gmail.com>
2022-09-21 15:09:11 +0430

    add some test 1401-6-30

--- test_src/test.py
+++ test_src/test.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.service import Service
 from time import sleep
 from selenium.webdriver.common.by import By
 import random
+from selenium.webdriver import ActionChains
+
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
@@ -22,7 +24,8 @@ driver.find_element(By.XPATH, '//a[text()="Phones & PDAs"]').click()
 # sleep(1)
 
 # click on iphone and show
-driver.find_element(By.XPATH, '//a[text()="iPhone"]').click()
+# driver.find_element(By.XPATH, '//a[text()="iPhone"]').click()
+
 # sleep(1)
 
 
@@ -32,17 +35,34 @@ driver.find_element(By.XPATH, '//a[text()="iPhone"]').click()
 # driver.find_element(By.XPATH, '//button[@title="Close (Esc)"]').click()
 
 
-quantity = driver.find_element(By.XPATH, '//input[@name="quantity"]')
-quantity.click()
-sleep(0.5)
-quantity.clear()
-sleep(0.5)
-quantity.send_keys(2)
-sleep(0.5)
-quantity = driver.find_element(
-    By.XPATH, '//button[@id="button-cart"]').click()
-sleep(0.5)
+# quantity = driver.find_element(By.XPATH, '//input[@name="quantity"]').click()
+# # quantity.click()
+# sleep(0.5)
+# quantity.clear()
+# sleep(0.5)
+# quantity.send_keys(2)
+# sleep(0.5)
+# quantity = driver.find_element(
+#     By.XPATH, '//button[@id="button-cart"]').click()
+# sleep(0.5)
 
 
 laptop = driver.find_element(
     By.XPATH, '//a[text()="Laptops & Notebooks"]')
+
+action = ActionChains(driver)
+action.move_to_element(laptop).perform()
+
+
+laptops_2 = driver.find_element(
+    By.XPATH, '//a[text()="Show All Laptops & Notebooks"]').click()
+
+
+hp = driver.find_element(
+    By.XPATH, '//a[text()="HP LP3065"]').click()
+
+
+add_2_bbutton2 = driver.find_element(
+    By.XPATH, '//button[@id="button-cart"]')
+add_2_bbutton2.location_once_scrolled_into_view
+sleep(1)
